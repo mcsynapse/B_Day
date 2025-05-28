bc8b0efe69d18de312b372a54b1b1f19bf0c8348
MCSynapse <59708770+mcsynapse@users.noreply.github.com>
2025-05-28 23:14:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,42 +9,9 @@ window.onload = () => {
 
   // Auto start music with fade in
   const music = document.getElementById('bg-music');
-  music.volume = 0;
+  music.volume = 100;
   music.play();
-
-  let vol = 0;
-  const fadeIn = setInterval(() => {
-    if (vol < 1) {
-      vol += 0.05;
-      music.volume = Math.min(vol, 1);
-    } else {
-      clearInterval(fadeIn);
-    }
-  }, 200);
-
-  // Loop with fade out/in every 20s
-  setInterval(() => {
-    let fadeOut = setInterval(() => {
-      if (vol > 0) {
-        vol -= 0.05;
-        music.volume = Math.max(vol, 0);
-      } else {
-        clearInterval(fadeOut);
-        music.currentTime = 0;
-        music.play();
-
-        // Fade back in
-        let fadeInAgain = setInterval(() => {
-          if (vol < 1) {
-            vol += 0.05;
-            music.volume = Math.min(vol, 1);
-          } else {
-            clearInterval(fadeInAgain);
-          }
-        }, 200);
-      }
-    }, 200);
-  }, 20000);
+  
 };
 
 confetti({
